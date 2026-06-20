6ebd394c6ee37e9d218d8a041b802ad2db77ae0e
Sameer Gupta <sameerguptabg2007@gmail.com>
2026-06-20 19:27:40 +0530

    Day 1 evening: madlibs game

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+#Madlibs game
+#word game where the player is prompted for a list of words to substitute for blanks in a story, before reading the - often comical or nonsensical - story aloud.
+
+print("Welcome to the Madlibs Game!")
+
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb (ing): ")
+verb2 = input("Enter another verb (ing): ")
+adjective2 = input("Enter another adjective: ")
+
+print(f"Today i went to a {adjective1} museum")
+print(f"In the museum, I saw  {noun1}")
+print(f"He was {verb1}ing and {verb2}ing")
+print(f"I saw him and felt {adjective2}")
